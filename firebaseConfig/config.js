c04994550e248101ccaeedba47fb67fddcc90f7d
Wriddhi Hazra <wriddhihazra@gmail.com>
2022-10-05 01:26:18 +0530

    Initial commit

---
+++ firebaseConfig/config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5RrxAbPckJpq3BOD-m1Kvr7SNhUD6FBE",
+  authDomain: "blog-site-465c1.firebaseapp.com",
+  projectId: "blog-site-465c1",
+  storageBucket: "blog-site-465c1.appspot.com",
+  messagingSenderId: "753739900982",
+  appId: "1:753739900982:web:8314bf25f34640720cd097",
+  measurementId: "G-CHYV6K39TN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
